47796bbf510c60020d3a2db5dcd198df3681d804
Noemi Romano <noemi.romano@protonmail.com>
2019-10-16 18:15:23 +0200

    importing d3

--- earthquakes.js
+++ earthquakes.js
@@ -1,9 +1,26 @@
 
-
-
-
 const url = "https://emidius.mi.ingv.it/fdsnws/event/1/query?starttime=1950-01-01T00:00:00&endtime=2014-12-31T23:59:59&orderby=time-asc&limit=4000&format=text";
+const width = 900;
+const height = 600;
 
+let projection = d3.geoMercator();
+
+let svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+let path = d3.geoPath()
+    .projection(projection);
+
+let g = svg.append("g");
+
+d3.json("world-110m2.json", function(error, topology) {
+    g.selectAll("path")
+      .data(topojson.object(topology, topology.objects.countries).geometries)
+    .enter()
+      .append("path")
+      .attr("d", path)
+        });
 
 
 // Create a timer
@@ -11,22 +28,6 @@ const url = "https://emidius.mi.ingv.it/fdsnws/event/1/query?starttime=1950-01-0
 //   redraw();  // call the function you created to update the chart
 // }, 1500);
 
-  var svg = d3.select("svg"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
-
-
-  var path = d3.geoPath();
-  var projection = d3.geoMercator()
-                     .scale(70)
-                     .center([0,20])
-                     .translate([width / 2, height / 2]);
-
-
-  var data = d3.map();
-  var colorScale = d3.scaleThreshold()
-                     .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-                     .range(d3.schemeBlues[7]);
 
 
 // Load data
