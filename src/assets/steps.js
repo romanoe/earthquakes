40f385abd8ab30fdd1f3ce7d70d27c91df79d8eb
romanoe <noemi.romano@protonmail.com>
2023-04-07 16:41:31 +0200

    refactor steps in another file

---
+++ src/assets/steps.js
@@ -0,0 +1,42 @@
+export let steps = [{
+  "id" : 1,
+  "name": "Terremoti in Italia",
+  "coordinates": [13.3995, 42.3498],
+  "img": "",
+  "caption": "",
+  "description": "I terremoti dell'Emilia nel 2012 sono stati frutto dello scontro della placca africana contro quella europea, creando una taglia lunga una quarantina di chilometri tra Ferrara et Modena. Si contano a oggi una ventina di morti e 300 feriti."
+}, {
+  "id" : 2,
+  "name": "Terremoti dell'Emilia",
+  "coordinates": [10.801449, 44.718074],
+  "img": "./img/emilia.jpg",
+  "caption": "La chiesa di San Martino di Tours a Buonacompra di Cento, crollata dopo le scosse del 20 maggio | Wikipedia",
+  "description": "I terremoti dell'Emilia nel 2012 sono stati frutto dello scontro della placca africana contro quella europea, creando una taglia lunga una quarantina di chilometri tra Ferrara et Modena. Si contano a oggi una ventina di morti e 300 feriti."
+}, {
+  "id" : 3,
+  "name": "Terremoti dell'Appennino",
+  "coordinates": [13.3995, 42.3498],
+  "img": "./img/aquila.jpg",
+  "caption": "Un ufficio del governo distrutto dal terremoto del 2009 a L'Aquila | Wikipedia",
+  "description": "L'Appennino centrale colleziona una serie di consistenti terremoti causata dallo stesso fenomeno dei terremoti dell'Emilia. Amatrice et l'Aquila sono state le città più colpite contando entrambe una centinaia di morti e migliaia di sfollati."
+
+},
+  {
+    "id" : 4,
+    "name": "Terremoto del Molise",
+    "coordinates": [14.963460381772917, 41.68703764959287],
+    "img": "./img/villaggio-provvisorio.jpg",
+    "caption": "Villaggio provvisorio a San Giuliano di Puglia | Primonumero",
+    "description": "Nel 2002, una forte scossa colpì varie città del basso Molise, tra cui San Giuliano di Puglia, dove morirono 27 bambini, 1 maestra et 2 bidelli sotto le macerie della scuola Jovine. Dopo varie indagini giudiziarie, furono accusati i progettisti, i costruttori e il sindaco dell'epoca per mancato rispetto delle norme. Furono costruiti villaggi provvisori per accogliere le migliaia di sfollati."
+
+  },
+  {
+    "id" : 5,
+    "name": "Terremoti del Basso Tirreno",
+    "coordinates": [14.859256, 38.691885],
+    "img": "./img/messina.avif",
+    "caption": "Le macerie a Messina dopo la grande scossa e lo tsunami, 1908 | Focus",
+    "description": "La zona del Basso Tirreno è conosciuta per i terremoti con ipocentro profondo, meno pericolosi di quelli superficiali. In questa zona ci troviamo in effetti in un piano di Benioff (crosta oceanica sprofonda sotto la crosta continentale) e si possono verificare terremoti fino a 500km di profondità. I terremoti constatati negli ultimi 20 anni non hanno causato danni comme nelle altre zone d'Italia ma sono stati risentiti fino in Calabria e in Sicilia. Una grande frequenza di terremoti profondi non esclude la possibilità di terremoti più volenti : nel 1908, Messina fu colpita da un terremoto che raggiunse una magnitudo 7.3 della scala Richter e che è considerato l'evento pìu violento in Europa dall'introduzione degli strumenti di misurazione."
+
+  }
+]
--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -6,43 +6,13 @@
     import {draw} from "svelte/transition";
     import {Column, Grid, Row} from "carbon-components-svelte";
     import {zoom, zoomIdentity} from "d3-zoom";
+    import {steps} from '../assets/steps.js';
 
     // Scroll
     import Scroll from "./Scrolly.svelte";
 
     // Scroll steps
     let currentStep;
-    const steps = [{
-        name: "Terremoti dell'Emilia",
-        coordinates: [10.801449, 44.718074],
-        img: "./img/emilia.jpg",
-        caption: "La chiesa di San Martino di Tours a Buonacompra di Cento, crollata dopo le scosse del 20 maggio | Wikipedia",
-        description: "I terremoti dell'Emilia nel 2012 sono stati frutto dello scontro della placca africana contro quella europea, creando una taglia lunga una quarantina di chilometri tra Ferrara et Modena. Si contano a oggi una ventina di morti e 300 feriti."
-    }, {
-        name: "Terremoti dell'Appennino",
-        coordinates: [13.3995, 42.3498],
-        img: "./img/aquila.jpg",
-        caption: "Un ufficio del governo distrutto dal terremoto del 2009 a L'Aquila | Wikipedia",
-        description: "L'Appennino centrale colleziona una serie di consistenti terremoti causata dallo stesso fenomeno dei terremoti dell'Emilia. Amatrice et l'Aquila sono state le città più colpite contando entrambe una centinaia di morti e migliaia di sfollati."
-
-    },
-        {
-            name: "Terremoto del Molise",
-            coordinates: [14.963460381772917, 41.68703764959287],
-            img: "./img/villaggio-provvisorio.jpg",
-            caption: "Villaggio provvisorio a San Giuliano di Puglia | Primonumero",
-            description: "Nel 2002, una forte scossa colpì varie città del basso Molise, tra cui San Giuliano di Puglia, dove morirono 27 bambini, 1 maestra et 2 bidelli sotto le macerie della scuola Jovine. Dopo varie indagini giudiziarie, furono accusati i progettisti, i costruttori e il sindaco dell'epoca per mancato rispetto delle norme. Furono costruiti villaggi provvisori per accogliere le migliaia di sfollati."
-
-        },
-        {
-            name: "Terremoti del Basso Tirreno",
-            coordinates: [14.859256, 38.691885],
-            img: "./img/messina.avif",
-            caption: "Le macerie a Messina dopo la grande scossa e lo tsunami, 1908 | Focus",
-            description: "La zona del Basso Tirreno è conosciuta per i terremoti con ipocentro profondo, meno pericolosi di quelli superficiali. In questa zona ci troviamo in effetti in un piano di Benioff (crosta oceanica sprofonda sotto la crosta continentale) e si possono verificare terremoti fino a 500km di profondità. I terremoti constatati negli ultimi 20 anni non hanno causato danni comme nelle altre zone d'Italia ma sono stati risentiti fino in Calabria e in Sicilia. Una grande frequenza di terremoti profondi non esclude la possibilità di terremoti più volenti : nel 1908, Messina fu colpita da un terremoto che raggiunse una magnitudo 7.3 della scala Richter e che è considerato l'evento pìu violento in Europa dall'introduzione degli strumenti di misurazione."
-
-        }
-    ];
 
 
     // Earthquakes API url
@@ -54,17 +24,18 @@
     let cities = [];
     let temporalEarthquakes = [];
 
+
     // SVG properties
     const margin = {top: 50, right: 50, bottom: 0, left: 50},
         w = window.innerWidth / 2 - margin.left - margin.right,
         h = window.innerHeight - margin.top - margin.bottom;
 
 
+
     //Define map projection and path
     const projection = geoMercator()
         .translate([w / 2 - 500, 2400])
         .scale([2500]);
-
     const path = geoPath().projection(projection)
 
 
@@ -84,7 +55,7 @@
 
         let tooltip = document.getElementById(source_id);
         tooltip.style.display = "block";
-        tooltip.style.left = event.layerX + 10 +'px';
+        tooltip.style.left = event.layerX + 10 + 'px';
         tooltip.style.top = event.layerY + 10 + 'px';
     }
     const hideTooltip = (event, source_id) => {
@@ -95,14 +66,14 @@
     const initZoom = () => {
         select('svg')
             .call(mapZoom)
-    .on("wheel.zoom", null);;
+            .on("wheel.zoom", null);
+        ;
     }
 
 
-    const zoomTo = (coordinates) => {
-        select("svg").transition().duration(4000).call(mapZoom.transform, zoomIdentity.translate(w / 2, h / 2).scale(5).translate(-projection(coordinates)[0], -projection(coordinates)[1]));
+    const zoomTo = (coordinates, scale) => {
+        select("svg").transition().duration(4000).call(mapZoom.transform, zoomIdentity.translate(w / 2, h / 2).scale(scale).translate(-projection(coordinates)[0], -projection(coordinates)[1]));
     }
-
 
     // Fetch data and initialize zoom
     onMount(async () => {
@@ -116,18 +87,19 @@
 
             initZoom();
 
-
         }
     )
 
     $: if (currentStep == 0) {
-        zoomTo(steps.map(d => d.coordinates)[0]);
+        zoomTo(steps.map(d => d.coordinates)[0], 1.3);
     } else if (currentStep == 1) {
-        zoomTo(steps.map(d => d.coordinates)[1]);
+        zoomTo(steps.map(d => d.coordinates)[1], 5);
     } else if (currentStep == 2) {
-        zoomTo(steps.map(d => d.coordinates)[2]);
+        zoomTo(steps.map(d => d.coordinates)[2], 5);
     } else if (currentStep == 3) {
-        zoomTo(steps.map(d => d.coordinates)[3]);
+        zoomTo(steps.map(d => d.coordinates)[3], 5);
+    } else if (currentStep == 4) {
+        zoomTo(steps.map(d => d.coordinates)[4], 5);
     }
 
 
@@ -142,7 +114,7 @@
                     <svg width={w} height={h}>
 
                         <!--Italy regions shapes -->
-                        <g fill="white" stroke="grey" stroke-width="0.1" class="no-interaction">
+                        <g fill="white" stroke="grey" stroke-width="0.4">
                             {#each borders as border, i}
                                 <path d={path(border)} in:draw={{ delay: i * 50, duration: 1500 }}/>
                             {/each}
@@ -155,15 +127,15 @@
                                 <circle
 
                                         r="{Math.pow(1.1,earthquake.properties.mag)}"
-                                cx={projection([earthquake.geometry.coordinates[0], earthquake.geometry.coordinates[1]])[0]}
-                                cy="{projection([earthquake.geometry.coordinates[0], earthquake.geometry.coordinates[1]])[1]}"
-                                fill="#F12526" stroke-width="1" opacity="0.1"
+                                        cx={projection([earthquake.geometry.coordinates[0], earthquake.geometry.coordinates[1]])[0]}
+                                        cy="{projection([earthquake.geometry.coordinates[0], earthquake.geometry.coordinates[1]])[1]}"
+                                        fill="#F12526" stroke-width="1" opacity="0.1"
                                         on:mouseover={showTooltip(event, earthquake.properties.source_id)}
                                         on:mouseout={hideTooltip(event, earthquake.properties.source_id)}
                                         on:blur={hideTooltip(event, earthquake.properties.source_id)}
                                         on:focus={showTooltip(event, earthquake.properties.source_id)}>
 
-                                        </circle>
+                                </circle>
                             {/each}
 
 
@@ -231,6 +203,7 @@
                 <section>
                     <Scroll bind:value={currentStep}>
                         {#each steps as step, i}
+
                             <div class="step" class:active={currentStep === i}>
                                 <div class="step-content">
                                     <h1>{step.name}</h1>
@@ -257,13 +230,9 @@
 
 <style>
 
-    .no-interaction {
-        pointer-events: none;
-    }
-
     h1 {
         margin: 30px;
-        font-family: Courier;
+        font-family: "Roboto";
         color: #F12526;
     }
 
